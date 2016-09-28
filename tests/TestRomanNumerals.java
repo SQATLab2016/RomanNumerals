f170eeaec3e38594a183be0f6bd419b115eb4ab0
somename <someemail>
2016-09-28 17:24:13 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -18,7 +18,7 @@ public class TestRomanNumerals {
 	
 	@Test(expected=NumberFormatException.class)
 	public void testConvertIntegerFourX_Throw_NumberFormatException() {
-		mNumerals.convertToInteger("XXX");
+		mNumerals.convertToInteger("XXXX");
 	}
 
 	@Test
