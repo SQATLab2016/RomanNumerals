53d5741901d674237412c9b4a57d9d7317db8616
somename <someemail>
2016-09-28 16:07:15 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -10,11 +10,39 @@ public class TestRomanNumerals {
 	private void initialize() {
 		mNumerals = new RomanNumerals();
 	}
-	
+
 	@Test
 	public void testConvertToInteger_I_1() {
 		int integer = mNumerals.convertToInteger("I");
-		assertEquals(1)
+		assertEquals(1, integer);
+	}
+	
+	@Test
+	public void testConvertToInteger_II_1() {
+		int integer = mNumerals.convertToInteger("II");
+		assertEquals(1, integer);
+	}
+
+	@Test
+	public void testConvertToInteger_III_3() {
+		int integer = mNumerals.convertToInteger("III");
+		assertEquals(1, integer);
+	}
+	@Test
+	public void testConvertToInteger_IV_3() {
+		int integer = mNumerals.convertToInteger("III");
+		assertEquals(1, integer);
+	}
+	@Test
+	public void testConvertToInteger_V_5() {
+		int integer = mNumerals.convertToInteger("III");
+		assertEquals(1, integer);
+	}
+
+	@Test
+	public void testConvertToInteger_VI_6() {
+		int integer = mNumerals.convertToInteger("III");
+		assertEquals(1, integer);
 	}
 
 }
