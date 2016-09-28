ae22e5a8771625978c87553a92365631d5ae5396
Ari Ruokamo <ari.j.ruokamo@boogiesoftware.com>
2016-09-28 18:46:24 +0300

    Implemented simple converter from roman to arabic. Works with just simple numbers at the moment, loop not implemented.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,31 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+		int result = 0;
+    	if (romanNum.equals("I")) {
+    		result = 1;
+   		} else if (romanNum.equals("V")) {
+   			result = 5;
+   		} else if (romanNum.equals("X")) {
+    		result = 10;
+    	} else if (romanNum.equals("L")) {
+    		result = 50;
+   		} else if (romanNum.equals("C")) {
+   			result = 100;
+   		} else if (romanNum.equals("D")) {
+   			result = 500;
+   		} else if (romanNum.equals("M")) {
+   			result = 1000;
+   		}
+    	return result;
+    	
+    	// Change the above to loop the string char by char to be able to 
+    	// calculate longer roman numbers
+    	// 
+/*    	for (int i = romanNum.length(); i >= 0; i--) {
+    		char prime =  romanNum.charAt(i);
+    		// Not implemented, time is up
+    	}
+*/    	
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -4,9 +4,61 @@ import org.junit.Test;
 
 public class TestRomanNumerals {
 
+	// Test "prime" values
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOnes_one() {
+		RomanNumerals rn = new RomanNumerals();
+		int value = rn.convertToInteger("I");
+		assertEquals(1, value);
 	}
 
+	@Test
+	public void testFive() {
+		RomanNumerals rn = new RomanNumerals();
+		int value = rn.convertToInteger("V");
+		assertEquals(5, value);
+	}
+
+	@Test
+	public void testTen() {
+		RomanNumerals rn = new RomanNumerals();
+		int value = rn.convertToInteger("X");
+		assertEquals(10, value);
+	}
+	
+	@Test
+	public void testFifty() {
+		RomanNumerals rn = new RomanNumerals();
+		int value = rn.convertToInteger("L");
+		assertEquals(50, value);
+	}
+
+	@Test
+	public void testHundred() {
+		RomanNumerals rn = new RomanNumerals();
+		int value = rn.convertToInteger("C");
+		assertEquals(100, value);
+	}
+
+	@Test
+	public void testFiveHundred() {
+		RomanNumerals rn = new RomanNumerals();
+		int value = rn.convertToInteger("D");
+		assertEquals(500, value);
+	}
+	
+	@Test
+	public void testThousand() {
+		RomanNumerals rn = new RomanNumerals();
+		int value = rn.convertToInteger("M");
+		assertEquals(1000, value);
+	}
+	
+	// Test "odd, under and over"
+	@Test
+	public void testFour() {
+		RomanNumerals rn = new RomanNumerals();
+		int value = rn.convertToInteger("IV");
+		assertEquals(4, value);
+	}
 }
