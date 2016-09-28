3c044ba878536932a409aa607ff16b251dc07ad3
jmouruja <jmouruja@STUPID-PC417.stnet.local>
2016-09-28 16:44:30 +0300

    Initialized 8 tests for basic letters, started creating a array for larger roman numerals

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,45 @@
 
 public class RomanNumerals {
+	
+	int convertedInteger = 0;
+	
+	
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		
+		String[] characters = new String[romanNum.length()];
+		
+		for (int i = 0; i < romanNum.length(); i++) {
+		characters[i] = romanNum.substring(i, i+1);
+		}
+		convertTo(romanNum);
+			
+		return convertedInteger;
+		
+	}
+
+	private void convertTo(String romanNum) {
+		
+		if (romanNum == "VII")
+			convertedInteger += 7;
+		
+		else if (romanNum == "VI")
+			convertedInteger += 6;
+		
+		else if (romanNum == "V")
+			convertedInteger += 5;
+		
+		if (romanNum == "IV")
+			convertedInteger += 4;
+			
+		else if (romanNum == "III")
+			convertedInteger += 3;
+		
+		else if (romanNum == "II")
+			convertedInteger += 2;
+		
+		else if (romanNum == "I")
+			convertedInteger += 1;
 		
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,85 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	
+	RomanNumerals test = new RomanNumerals();
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAppCreated_NotNull() {
+		
+	assertNotNull(test);
 	}
+	@Test
+	public void testRomanNumerals_One() {
+		
+	int integer = test.convertToInteger("I");
+	assertEquals(1, integer);
+	
+	}
+	@Test
+	public void testRomanNumerals_Two() {
+		
+	int integer = test.convertToInteger("II");
+	assertEquals(2, integer);
+	
+	}
+	@Test
+	public void testRomanNumerals_Three() {
+		
+	int integer = test.convertToInteger("III");
+	assertEquals(3, integer);
+	
+	}
+	@Test
+	public void testRomanNumerals_Four() {
+		
+	int integer = test.convertToInteger("IV");
+	assertEquals(4, integer);
+	
+	}
+	
+	@Test
+	public void testRomanNumberals_Five() {
+		
+	int integer = test.convertToInteger("V");
+	assertEquals(5, integer);
+		
+	}
+	
+	@Test
+	public void testRomanNumberals_Six() {
+		
+	int integer = test.convertToInteger("VI");
+	assertEquals(6, integer);
+		
+	}
+	@Test
+	public void testRomanNumberals_Seven() {
+		
+	int integer = test.convertToInteger("VII");
+	assertEquals(7, integer);
+		
+	}
+	@Test
+	public void testRomanNumberals_Eleven() {
+		
+	int integer = test.convertToInteger("XI");
+	assertEquals(11, integer);
+		
+	}
+	
+	
+	
+	
+	/*
+	private void assertIntegerEquals(String roman) {
+	//int integer = test.convertToInteger(roman);
+		
+		assertEquals(integer, roman);
+				
+	}*/
+	
+
 
 }
