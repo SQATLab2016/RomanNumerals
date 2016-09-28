8173abeeb0ba4f68f54dbd9121d0001913576ebc
somename <someemail>
2016-09-28 16:15:31 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,6 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
+		Pattern p = Pattern.compile("V{2,}");
+		Matcher m = p.matcher(romanNum);
+		if (m.find()) {
+			throw new IllegalArgumentException();
+		}
+		
 		if (romanNum.equals("I")) {
 			return 1;
 		} else if (romanNum.equals("II")) {
