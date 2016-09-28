2301224cb794af2982c6bc990e61478d36310e39
somename <someemail>
2016-09-28 17:31:51 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -16,7 +16,8 @@ public class RomanNumerals {
 				}
 			}
 			
-			if (duplicates >= 3 && romanNum.charAt(i) == 'X') {
+			if (duplicates >= 3 && (romanNum.charAt(i) == 'I' ||
+					romanNum.charAt(i) == 'X')) {
 				throw new NumberFormatException("Too many duplicate numeral character");
 			}
 			
