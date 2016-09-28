00aa8aba36d5516af99bb23fd8d82e29fafeaa1e
somename <someemail>
2016-09-28 16:16:07 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,12 +2,13 @@
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		switch(romanNum)
-		case "I":
-		case "V":
-		case "X":
-		case "L":
-		case "C":
+		case "I": return 1;
+		case "V": return 5;
+		case "X": return 10;
+		case "L": return 50;
+		case "C": return 100;
 		case "D": return 500;
 		case "M": return 1000;
+		default return -1;
 	}
 }
