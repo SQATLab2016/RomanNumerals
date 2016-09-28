40db15fb152e38e2dc6bd772c7db79aed62ef0b0
Olli Hiekkaranta <Olli Hiekkaranta>
2016-09-28 16:42:57 +0300

    add roman number conversion tests and logic

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,67 @@
 
 public class RomanNumerals {
+	private static final char I = 'I';
+	private static final char V = 'V';
+	private static final char X = 'X';
+	private static final char L = 'L';
+	private static final char C = 'C';
+	private static final char D = 'D';
+	private static final char M = 'M';
+
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
-		return 0;
+		int value = 0;
+		int stringLength = romanNum.length();
+		char currentChar, nextChar;
+		
+		if (stringLength > 0)
+		{
+			for (int i = stringLength - 1; i >= 0; i--) {
+				currentChar = romanNum.charAt(i);
+				if (i < stringLength - 1) {
+					nextChar = romanNum.charAt(i + 1);
+					if (currentChar == nextChar) {
+						value += getCharValue(currentChar);
+					}
+					else if (getCharValue(currentChar) < getCharValue(nextChar)) {
+						value -= getCharValue(currentChar);
+					}
+					else if (getCharValue(currentChar) > getCharValue(nextChar)) {
+						value += getCharValue(currentChar);
+					}
+				}
+				else {
+					value += getCharValue(currentChar);
+				}
+			}
+		}
+		return value;
 		
 	}
+	
+	private int getCharValue(char romanNum) {
+		if (romanNum == I) {
+			return 1;
+		}
+		else if (romanNum == V) {
+			return 5;
+		}
+		else if (romanNum == X) {
+			return 10;
+		}
+		else if (romanNum == L) {
+			return 50;
+		}
+		else if (romanNum == C) {
+			return 100;
+		}
+		else if (romanNum == D) {
+			return 500;
+		}
+		else if (romanNum == M) {
+			return 1000;
+		}
+		return 0;
+	}
+
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,97 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	
+	private RomanNumerals rn = new RomanNumerals();
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test public void testRomanNumerals_emptyString() {
+		int num = rn.convertToInteger("");
+		assertEquals(0, num);
+	}
+	
+	@Test public void testRomanNumerals_I_1() {
+		int num = rn.convertToInteger("I");
+		assertEquals(1, num);
+	}
+	
+	@Test public void testRomanNumerals_II_2() {
+		int num = rn.convertToInteger("II");
+		assertEquals(2, num);
+	}
+	
+	@Test public void testRomanNumerals_III_3() {
+		int num = rn.convertToInteger("III");
+		assertEquals(3, num);
+	}
+	
+	@Test public void testRomanNumerals_V_5() {
+		int num = rn.convertToInteger("V");
+		assertEquals(5, num);
+	}
+	
+	@Test public void testRomanNumerals_IV_4() {
+		int num = rn.convertToInteger("IV");
+		assertEquals(4, num);
+	}
+	
+	@Test public void testRomanNumerals_VI_6() {
+		int num = rn.convertToInteger("VI");
+		assertEquals(6, num);
+	}
+	
+	@Test public void testRomanNumerals_X_10() {
+		int num = rn.convertToInteger("X");
+		assertEquals(10, num);
+	}
+	
+	@Test public void testRomanNumerals_L_50() {
+		int num = rn.convertToInteger("L");
+		assertEquals(50, num);
+	}
+	
+	@Test public void testRomanNumerals_C_100() {
+		int num = rn.convertToInteger("C");
+		assertEquals(100, num);
+	}
+	
+	@Test public void testRomanNumerals_D_500() {
+		int num = rn.convertToInteger("D");
+		assertEquals(500, num);
+	}
+	
+	@Test public void testRomanNumerals_M_1000() {
+		int num = rn.convertToInteger("M");
+		assertEquals(1000, num);
+	}
+	
+	@Test public void testRomanNumerals_XX_20() {
+		int num = rn.convertToInteger("XX");
+		assertEquals(20, num);
+	}
+	
+	@Test public void testRomanNumerals_XC_90() {
+		int num = rn.convertToInteger("XC");
+		assertEquals(90, num);
+	}
+	
+	@Test public void testRomanNumerals_LXX_70() {
+		int num = rn.convertToInteger("LXX");
+		assertEquals(70, num);
+	}
+	
+	@Test public void testRomanNumerals_CM_900() {
+		int num = rn.convertToInteger("CM");
+		assertEquals(900, num);
+	}
+	
+	@Test public void testRomanNumerals_MCMLXXXIV_1984() {
+		int num = rn.convertToInteger("MCMLXXXIV");
+		assertEquals(1984, num);
+	}
+	
+	@Test public void testRomanNumerals_MMXIV_2014() {
+		int num = rn.convertToInteger("MMXIV");
+		assertEquals(2014, num);
 	}
 
 }
