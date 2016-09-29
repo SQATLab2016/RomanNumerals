8a13c2812c5f8a7c0c9ed268ddc0aef4a242fd18
somename <someemail>
2016-09-29 16:01:42 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -4,8 +4,30 @@ public class RomanNumerals {
 	public String RomanNumber;
 
 	public int convertToInteger(String romanNum) {
-		if (RomanNumber=="I"){
-			ArabicNumber++;
+		String[] RomanNumber = romanNum.split("");
+		int i=0;
+		for(i=0;i<RomanNumber.length;i++){
+			if (RomanNumber[i]=="I"){
+				ArabicNumber++;
+			}
+			if (RomanNumber[i]=="V"){
+				ArabicNumber=ArabicNumber+5;
+			}
+			if (RomanNumber[i]=="X"){
+				ArabicNumber=ArabicNumber+10;
+			}
+			if (RomanNumber[i]=="L"){
+				ArabicNumber=ArabicNumber+50;
+			}
+			if (RomanNumber[i]=="C"){
+				ArabicNumber=ArabicNumber+100;
+			}
+			if (RomanNumber[i]=="D"){
+				ArabicNumber=ArabicNumber+500;
+			}
+			if (RomanNumber[i]=="M"){
+				ArabicNumber=ArabicNumber+1000;
+			}
 		}
 		return ArabicNumber;
 		
