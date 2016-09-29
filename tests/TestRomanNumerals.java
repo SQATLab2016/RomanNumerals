b477c561e7d416b1a36e9a624e38c09bd3398576
somename <someemail>
2016-09-29 15:39:30 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 public class TestRomanNumerals {
 
+	RomanNumerals romanNumerals= new RomanNumerals();
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
@@ -13,14 +15,13 @@ public class TestRomanNumerals {
 	}
 
 	@Test
-	public void test_FirstRule() {
+	public void test_CherFor3I() {
 		//The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	
 		//		at	most	3	times	in	a	row.	
 
-		
-		
-		
-		fail("Not yet implemented");
+		String num="MCMLXXXIV";
+				
+		assertTrue(cherFor3I(num));
 	}
 
 }
