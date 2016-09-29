a813f49d722f87b537035191fb5d618d5b66eb59
somename <someemail>
2016-09-29 16:06:54 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -30,6 +30,11 @@ public class TestRomanNumerals {
 	public void test_ConvertToIntegerValueIV() {
 		assertRomanValue(4, "IV");
 	}
+	
+	@Test
+	public void test_ConvertToIntegerValueX() {
+		assertRomanValue(10, "X");
+	}
 
 	private void assertRomanValue(int expected, String testValue) {
 		int value = numerals.convertToInteger(testValue);
