6f0857394d1f0808d88d23320416ea7a1b54e5ca
Santa María Purísima <Santa Mara Pursima@10.20.206.162>
2016-09-29 16:56:03 +0300

    TDD

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -8,5 +8,35 @@ public class TestRomanNumerals {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testTwo(){	
+		
+		assertEquals("Check",2,RomanNumerals.convert("II"));
+	}
+	
+	@Test
+	public void testSpecialLetters(){	
+		
+		assertTrue("Check",RomanNumerals.checkCharacters("VI"));
+	}
+
+	@Test
+	public void testRepite3(){	
+		
+		assertFalse("Check",RomanNumerals.checkRepetition3("VIIII"));
+	}
+	
+	@Test
+	public void testRepite2(){	
+		
+		assertFalse("Check",RomanNumerals.checkRepetition2("VIVIV"));
+	}
+	
+	@Test
+	public void testSubstraction(){	
+		
+		assertEquals("Check",'I',RomanNumerals.checkSubstraction("IX"));
+	}
 
 }
