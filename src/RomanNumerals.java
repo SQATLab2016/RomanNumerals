f98189016caf3ace49b5cdcb9e2153c160324e19
somename <someemail>
2016-09-29 15:21:37 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -7,11 +7,14 @@ public class RomanNumerals {
 			}
 		
 		}
+		
+		if(romanNum == "I")
+			return 1;		
+		else if (romanNum == "II")
+			return 2;
+		
 		for(int c = 0; c < romanNum.length();c++) {
-			if(romanNum == "I")
-				return 1;		
-			else if (romanNum == "II")
-				return 2;
+			
 		}
 		
 		return 0;
