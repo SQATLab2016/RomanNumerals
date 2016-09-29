8319049828188534b7af80226ba91f62b62c8f8c
somename <someemail>
2016-09-29 15:29:46 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,6 +1,7 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) throws RomanNumeralsException{
+		String s = romanNum;
 		for(char c : romanNum.toCharArray()) {
 			if(c != 'I' || c != 'V' || c != 'X' || c != 'L' || c != 'C' || c != 'D' || c != 'M') {
 				throw new RomanNumeralsException("Roman Numeral to be converted isn't valid");
@@ -12,13 +13,16 @@ public class RomanNumerals {
 			return 1;		
 		}
 		else if (romanNum.equals("II")) {
+			System.out.println("" + romanNum);
 			return 2;
 		}
 		else if (romanNum.equals("III")) {
+			System.out.println("" + romanNum);
 			return 3;
 		}
-		else
+		else {
 			System.out.println("" + romanNum);
+		}
 		
 		for(int c = 0; c < romanNum.length();c++) {
 			
