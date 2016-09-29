0e897feb55e724b48bcfd042012ff1b071b1907c
somename <someemail>
2016-09-29 16:09:39 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -7,27 +7,14 @@ public class RomanNumerals {
 		String[] RomanNumber = romanNum.split("");
 		int i=0;
 		for(i=0;i<RomanNumber.length;i++){
-			if (RomanNumber[i]=="I"){
-				ArabicNumber=ArabicNumber+1;
-			}
-			if (RomanNumber[i]=="V"){
-				ArabicNumber=ArabicNumber+5;
-			}
-			if (RomanNumber[i]=="X"){
-				ArabicNumber=ArabicNumber+10;
-			}
-			if (RomanNumber[i]=="L"){
-				ArabicNumber=ArabicNumber+50;
-			}
-			if (RomanNumber[i]=="C"){
-				ArabicNumber=ArabicNumber+100;
-			}
-			if (RomanNumber[i]=="D"){
-				ArabicNumber=ArabicNumber+500;
-			}
-			if (RomanNumber[i]=="M"){
-				ArabicNumber=ArabicNumber+1000;
-			}
+			System.out.println(RomanNumber[i]);
+			if (RomanNumber[i]=="I"){ArabicNumber=ArabicNumber+1;}
+			if (RomanNumber[i]=="V"){ArabicNumber=ArabicNumber+5;}
+			if (RomanNumber[i]=="X"){ArabicNumber=ArabicNumber+10;}
+			if (RomanNumber[i]=="L"){ArabicNumber=ArabicNumber+50;}
+			if (RomanNumber[i]=="C"){ArabicNumber=ArabicNumber+100;}
+			if (RomanNumber[i]=="D"){ArabicNumber=ArabicNumber+500;}
+			if (RomanNumber[i]=="M"){ArabicNumber=ArabicNumber+1000;}
 		}
 		return ArabicNumber;
 		
