602f4a956aca597e7c38428dad927b9c2df47f10
somename <someemail>
2016-09-29 15:37:02 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -7,6 +7,20 @@ public class TestRomanNumerals {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+
+
+
+	}
+
+	@Test
+	public void test_FirstRule() {
+		//The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	
+		//		at	most	3	times	in	a	row.	
+
+		
+		
+		
+		fail("Not yet implemented");
 	}
 
 }
