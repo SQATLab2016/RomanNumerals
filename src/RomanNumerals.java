0aa361f731cbccf1f9102396a8e65405cff20c38
somename <someemail>
2016-09-29 15:41:21 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,14 +1,30 @@
 
 public class RomanNumerals {
-	public RomanNumerals(String string) {
+	public RomanNumerals(){
 		// TODO Auto-generated constructor stub
+		//Tables for roman numerals and numbers should be added here
 	}
 
 	public int convertToInteger(String romanNum) throws RomanNumeralsException {
 		// To be Implemented
+		//Test for too many "I, X, C, M" returns true if too many
+		if(testRepeatedTooManyTimes(romanNum)) throw new RomanNumeralsException();
+		
+		if(romanNum == "IIII") throw new RomanNumeralsException();
 		if(romanNum == "I") return 1;
 		
 		return 0;
+	}
+	
+	//Return false if one of the letters is repeated too many times
+	public boolean testRepeatedTooManyTimes(String roman){
+		//Test if there is a possibility for too many of the same number
+		if(roman.length() < 4 ) return false;
+		
+		//Test the obvious
+		if(roman == "IIII") return true;
+		
+		return false;
 	}
 	
 	
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,21 +5,22 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		RomanNumerals rm = new RomanNumerals("I");
+	public void test() throws RomanNumeralsException {
+		RomanNumerals rm = new RomanNumerals();
 		assertNotNull(rm);
 	}
 	
 	@Test
-	public void testReturnWith_I() {
-		RomanNumerals rm = new RomanNumerals("I");
+	public void testReturnWith_I() throws RomanNumeralsException{
+		RomanNumerals rm = new RomanNumerals();
 		int number = rm.convertToInteger("I");
 		assertEquals("I was not 1", 1, number);
 	}
 	
-	@Test
+	@Test (expected = RomanNumeralsException.class)
 	public void testMoreThanThree_I_s() throws RomanNumeralsException {
-		
+		RomanNumerals rm = new RomanNumerals();
+		rm.convertToInteger("IIII");
 	}
 
 }
