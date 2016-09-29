033ca83d420adef63eb57a4995c66b18217110e3
gruizsan <gruizsan@STUPID-PC723.stnet.local>
2016-09-29 16:53:31 +0300

    Not finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,119 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+	static int counterI = 0;
+	static int counterX = 0;
+	static int counterC = 0;
+	static int counterM = 0;
+	static int counterV = 0;
+	static int counterL = 0;
+	static int counterD = 0;
+	
+	public static int convertToInteger(String romanNum) {
+		int num = 0;
+		int sum  = 0;
+		int prevNum = 0;
+
+		char lastOperation = ' ';
+		for(int i = romanNum.length()-1; i>=0; i--){
+			num = basicConvert(romanNum.charAt(i));
+			
+			if(lastOperation == '-' && num == prevNum) return -1;
+			
+			if(check1NumbersAmount(num) == -1) return -1;
+			if(check5NumbersAmount(num) == -1) return -1;
+			
+			if(num>=prevNum){
+				sum = sum + num;
+				lastOperation = '+';
+			}
+			else{
+				if(num == 5 || num == 50 || num == 500) return -1;
+				
+				if(num == 1 && prevNum > 10) return -1;
+				if(num == 10 && prevNum > 100) return -1;
+				
+				sum = sum - num;
+				lastOperation = '-';
+			}
+			prevNum = num;
+		}
+		restartCounters();
+		return sum;
 		
+	}
+	
+	private static void restartCounters(){
+		counterI = 0;
+		counterX = 0;
+		counterC = 0;
+		counterM = 0;
+		counterV = 0;
+		counterL = 0;
+		counterD = 0;
+	}
+	
+	private static int check1NumbersAmount(int num){
+		switch(num){
+		case 1: 
+			counterI++;
+			if(counterI > 3) return -1;
+			break;
+		case 10: 
+			counterX++;
+			if(counterX > 3) return -1;
+			break;
+		case 100: 
+			counterC++;
+			if(counterC > 3) return -1;
+			break;
+		case 1000: 
+			counterM++;
+			if(counterM > 3) return -1;
+			break;
+		default:
+			return 0;
+		}
+		return 0;
+	}
+	
+	private static int check5NumbersAmount(int num){
+		switch(num){
+		case 5: 
+			counterV++;
+			if(counterV > 1) return -1;
+			break;
+		case 50: 
+			counterL++;
+			if(counterL > 1) return -1;
+			break;
+		case 500: 
+			counterD++;
+			if(counterD > 1) return -1;
+			break;
+		default:
+			return 0;
+		
+		}
+		return 0;
+	}
+	
+	private static int basicConvert(char romanNum){
+		switch (romanNum){
+		case 'I':
+			return 1;
+		case 'V':
+			return 5;
+		case 'X':
+			return 10;
+		case 'L':
+			return 50;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+		default: return -1;
+		}
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,147 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_I_1() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("I");
+		//Assert
+		assertEquals(1, num);
+	}
+	
+	@Test
+	public void test_V_5() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("V");
+		//Assert
+		assertEquals(5, num);
+	}
+	
+	@Test
+	public void test_X_10() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("X");
+		//Assert
+		assertEquals(10, num);
+	}
+	
+	@Test
+	public void test_L_50() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("L");
+		//Assert
+		assertEquals(50, num);
+	}
+	
+	@Test
+	public void test_C_100() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("C");
+		//Assert
+		assertEquals(100, num);
+	}
+	
+	@Test
+	public void test_D_500() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("D");
+		//Assert
+		assertEquals(500, num);
+	}
+	
+	@Test
+	public void test_M_1000() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("M");
+		//Assert
+		assertEquals(1000, num);
+	}
+	
+	@Test
+	public void test_II_2() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("II");
+		//Assert
+		assertEquals(2, num);
+	}
+	
+	@Test
+	public void test_IV_4() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("IV");
+		//Assert
+		assertEquals(4, num);
+	}
+	
+	@Test
+	public void test_IIII_minus1() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("IIII");
+		//Assert
+		assertEquals(-1, num);
+	}
+	
+	@Test
+	public void test_XXXX_minus1() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("XXXX");
+		//Assert
+		assertEquals(-1, num);
+	}
+	
+	@Test
+	public void test_CCCC_minus1() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("CCCC");
+		//Assert
+		assertEquals(-1, num);
+	}
+	
+	@Test
+	public void test_MMMM_minus1() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("MMMM");
+		//Assert
+		assertEquals(-1, num);
+	}
+	
+	@Test
+	public void test_VMV_minus1() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("VMV");
+		//Assert
+		assertEquals(-1, num);
+	}
+	
+	@Test
+	public void test_IC_minus1() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("IC");
+		//Assert
+		assertEquals(-1, num);
+	}
+	
+	@Test
+	public void test_VX_minus1() {
+		//Arrange
+		//Act
+		int num = RomanNumerals.convertToInteger("VX");
+		//Assert
+		assertEquals(-1, num);
 	}
 
 }
