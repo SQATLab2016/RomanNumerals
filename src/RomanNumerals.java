fbfefb2ddf663c479d506417f74d35825ce5ba0c
somename <someemail>
2016-09-29 15:44:48 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,12 +1,50 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class RomanNumerals {
+	
 	public int convertToInteger(String romanNum) {
+		
 		if(romanNum.isEmpty()) {
 			return 0;
 		}
 		
+		int sum = 0;
 		
-		return 0;
+		for(int i = 0; i < romanNum.length(); i++) {
+			char roman = romanNum.charAt(i);
+			
+			int value = 0;
+			
+			switch(roman) {
+			
+			case 'I':
+				value = 1;
+			
+			case 'V':
+				value = 5;
+			
+			case 'X':
+				value = 10;
+				
+			case 'L':
+				value = 50;
+			
+			case 'C':
+				value = 100;
+			
+			case 'D':
+				value = 500;
+				
+			case 'M':
+				value = 1000;
+				
+				
+			}
+			sum = sum + value;
+		}
+		
+		return sum;
 		
 	}
 }
