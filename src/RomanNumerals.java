a49c8eb634d0f905b2a7780c594bd2c2fda86c55
somename <someemail>
2016-09-29 14:49:54 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,6 +1,12 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
+	public int convertToInteger(String romanNum) throws RomanNumeralsException{
+		for(char c : romanNum.toCharArray()) {
+			if(Character.isDigit(c)) {
+				throw new RomanNumeralsException("Roman Numeral to be converted isn't valid");
+			}
+		
+		}
 		if(romanNum == "I")
 			return 1;
 		else 
