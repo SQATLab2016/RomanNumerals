5ee73ceba186899adfd27f62c05535c131ffbba2
somename <someemail>
2016-09-29 14:39:11 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,5 +1,9 @@
 
 public class RomanNumerals {
+	public RomanNumerals(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
 		return 0;
