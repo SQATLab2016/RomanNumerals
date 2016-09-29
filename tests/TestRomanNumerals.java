85da47fe3d364c81c5069d722c4697e7c33cca6d
somename <someemail>
2016-09-29 15:50:52 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -19,10 +19,17 @@ public class TestRomanNumerals {
 	}
 	
 	@Test
-	public void test_Convert1Digit(){
+	public void test_Convert1(){
 		RomanNumerals.RomanNumber="I";
 		RomanNumerals.convertToInteger(RomanNumerals.RomanNumber);
 		assertEquals("",1,RomanNumerals.ArabicNumber);
+	}
+	
+	@Test
+	public void test_Convert_N_Digit(){
+		RomanNumerals.RomanNumber="MCMLXXXIV";
+		RomanNumerals.convertToInteger(RomanNumerals.RomanNumber);
+		assertEquals("",1984,RomanNumerals.ArabicNumber);
 	}
 	
 }
