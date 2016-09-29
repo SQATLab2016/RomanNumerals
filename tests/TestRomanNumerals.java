66aa3d99419d3df274404c1cfc87c09c7b44a752
somename <someemail>
2016-09-29 15:48:00 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -15,40 +15,40 @@ public class TestRomanNumerals {
 	}
 
 	@Test
-	public void test_CherFor3I() {
+	public void test_checkFor3I() {
 		//The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	
 		//		at	most	3	times	in	a	row.	
 		String num="MCMLXXXIV";
-		assertTrue(romanNumerals.cherFor3I(num));
+		assertTrue(romanNumerals.checkFor3I(num));
 	}
 	@Test
-	public void test_CherFor3X() {
+	public void test_checkFor3X() {
 		//The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	
 		//		at	most	3	times	in	a	row.	
 		String num="MCMLXXXIV";
-		assertTrue(romanNumerals.cherFor3X(num));
-	}
-
-	@Test
-	public void test_CherFor3C() {
-		//The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	
-		//		at	most	3	times	in	a	row.	
-		String num="MCMLXXXIV";
-		assertTrue(romanNumerals.cherFor3C(num));
-	}
-	@Test
-	public void test_CherFor3M() {
-		//The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	
-		//		at	most	3	times	in	a	row.	
-		String num="MCMLXXXIV";
-		assertTrue(romanNumerals.cherFor3M(num));
+		assertTrue(romanNumerals.checkFor3X(num));
 	}
 
 	@Test
-	public void test_CherForNoV() {
+	public void test_checkFor3C() {
+		//The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	
+		//		at	most	3	times	in	a	row.	
+		String num="MCMLXXXIV";
+		assertTrue(romanNumerals.checkFor3C(num));
+	}
+	@Test
+	public void test_checkFor3M() {
+		//The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	
+		//		at	most	3	times	in	a	row.	
+		String num="MCMLXXXIV";
+		assertTrue(romanNumerals.checkFor3M(num));
+	}
 
+	@Test
+	public void test_checkForNoV() {
 		//	The	symbols	'V',	'L',	and	'D'	can	never	be	repeated.	
-
+		String num="MCMLXXXIV";
+		assertTrue(romanNumerals.checkForNoV(num));
 
 
 
