16bb6e0f8eb9661030d51719aa2588b47753edb8
somename <someemail>
2016-09-29 16:25:38 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -6,14 +6,14 @@ public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		int i=0;
 		for(i=0;i<romanNum.length();i++){
-			String RomanNumber=romanNum.substring(i,i++);
-			if (RomanNumber=="I"){ArabicNumber=ArabicNumber+1;}
-			if (RomanNumber=="V"){ArabicNumber=ArabicNumber+5;}
-			if (RomanNumber=="X"){ArabicNumber=ArabicNumber+10;}
-			if (RomanNumber=="L"){ArabicNumber=ArabicNumber+50;}
-			if (RomanNumber=="C"){ArabicNumber=ArabicNumber+100;}
-			if (RomanNumber=="D"){ArabicNumber=ArabicNumber+500;}
-			if (RomanNumber=="M"){ArabicNumber=ArabicNumber+1000;}
+			char RomanNumber=romanNum.charAt(i);
+			if (RomanNumber=='I'){ArabicNumber=ArabicNumber+1;}
+			if (RomanNumber=='V'){ArabicNumber=ArabicNumber+5;}
+			if (RomanNumber=='X'){ArabicNumber=ArabicNumber+10;}
+			if (RomanNumber=='L'){ArabicNumber=ArabicNumber+50;}
+			if (RomanNumber=='C'){ArabicNumber=ArabicNumber+100;}
+			if (RomanNumber=='D'){ArabicNumber=ArabicNumber+500;}
+			if (RomanNumber=='M'){ArabicNumber=ArabicNumber+1000;}
 		}
 		return ArabicNumber;
 		
