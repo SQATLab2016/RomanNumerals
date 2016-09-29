96fa7ae90e04b1d07e67c987effa7358e8902d28
somename <someemail>
2016-09-29 16:25:26 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -11,10 +11,50 @@ public class RomanNumerals {
 		if(testRepeatedTooManyTimes(romanNum)) throw new RomanNumeralsException();
 		if(testSameRepeatedTooManyTimes(romanNum)) throw new RomanNumeralsException();
 		if(romanNum == "IIII") throw new RomanNumeralsException();
-		if(romanNum == "I") return 1;
 		
-		return 0;
+		//If every test pass then we can decode the roman to arabic
+		int number = decode(romanNum);
+		return number;
 	}
+	
+	//Roman numerals to decimal
+	//Source http://stackoverflow.com/questions/20313254/roman-numeral-to-number-conversion
+	private static int decodeSingle(char letter) {
+        switch (letter) {
+            case 'M':
+                return 1000;
+            case 'D':
+                return 500;
+            case 'C':
+                return 100;
+            case 'L':
+                return 50;
+            case 'X':
+                return 10;
+            case 'V':
+                return 5;
+            case 'I':
+                return 1;
+            default:
+                return 0;
+        }
+    }
+	
+	public static int decode(String roman) {
+        int result = 0;
+        String uRoman = roman.toUpperCase(); //case-insensitive
+        for (int i = 0; i < uRoman.length() - 1; i++) {//loop over all but the last character
+            if (decodeSingle(uRoman.charAt(i)) < decodeSingle(uRoman.charAt(i + 1))) {
+                result -= decodeSingle(uRoman.charAt(i));
+            } else {
+                result += decodeSingle(uRoman.charAt(i));
+            }
+        }
+        result += decodeSingle(uRoman.charAt(uRoman.length() - 1));
+        return result;
+    }
+	
+	
 	
 	//Return true if one of the letters is repeated too many times
 	public boolean testRepeatedTooManyTimes(String roman){
