9cab2deecfe87333ae282ac80de6f08684ac3db5
somename <someemail>
2016-09-30 17:00:07 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -11,6 +11,8 @@ public class RomanNumerals {
 		else if (romanNum.length() == 2) {
 			if (romanNum.substring(0,1).equals(romanNum.substring(1)))	
 				return 2*convertSingleRoman(romanNum.substring(0, 1));
+			else if ((romanNum.substring(0,1)<(romanNum.substring(1))
+				return convertSingleRoman(romanNum.substring(1,0)) - convertSingleRoman(romanNum.substring(0,1));	
 		}else if (romanNum == "II") {
 			return 1 + 1;
 		}else if (romanNum == "III"){
