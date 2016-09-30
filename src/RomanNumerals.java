ecc4e6dde2b100d323d8115707a7ba45b2942972
somename <someemail>
2016-09-30 17:03:06 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -13,6 +13,8 @@ public class RomanNumerals {
 				return 2*convertSingleRoman(romanNum.substring(0, 1));
 			else if ((convertSingleRoman(romanNum.substring(0,1))) < convertSingleRoman((romanNum.substring(1))))
 				return convertSingleRoman(romanNum.substring(1,0)) - convertSingleRoman(romanNum.substring(0,1));	
+			else if ((convertSingleRoman(romanNum.substring(0,1))) > convertSingleRoman((romanNum.substring(1))))
+				return convertSingleRoman(romanNum.substring(1,0)) + convertSingleRoman(romanNum.substring(0,1));	
 		}else if (romanNum == "II") {
 			return 1 + 1;
 		}else if (romanNum == "III"){
@@ -62,17 +64,17 @@ public class RomanNumerals {
 	private int convertSingleRoman(String roman) {
 		if (roman.equals("I"))
 			return 1;
-		else if (roman.equals("V")
+		else if (roman.equals("V"))
 			return 5;
-		else if (roman.equals("X")
+		else if (roman.equals("X"))
 			return 10;
-		else if (roman.equals("L")
+		else if (roman.equals("L"))
 			return 50;
-		else if (roman.equals("C")
+		else if (roman.equals("C"))
 			return 100;
-		else if (roman.equals("D")
+		else if (roman.equals("D"))
 			return 500;
-		else if (roman.equals("M")
+		else if (roman.equals("M"))
 			return 1000;
 		else
 			return 0;
