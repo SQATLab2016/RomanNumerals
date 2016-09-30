639f969eec6401bc2e1f18f5754d22b4b831ce46
somename <someemail>
2016-09-30 16:51:33 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -4,16 +4,18 @@ public class RomanNumerals {
 
 	
 	public int convertToInteger(String romanNum) {
-		if (romanNum == "I"){
+		
+		
+		if (romanNum.length() == 1)
 			return convertSingleRoman(romanNum);
+		else if (romanNum.length() == 2) {
+			if (romanNum.substring(0,1).equals(romanNum.substring(1)))	
 		}else if (romanNum == "II") {
 			return 1 + 1;
 		}else if (romanNum == "III"){
 			return 1 + 1 + 1;
 		}else if (romanNum ==  "IV"){
 			return 5 - 1;
-		}else if (romanNum == "V"){
-			return convertSingleRoman(romanNum);
 		}else if (romanNum == "VI"){
 			return 5 + 1;
 		}else if (romanNum == "VII"){
@@ -22,16 +24,12 @@ public class RomanNumerals {
 			return 5 + 1 + 1 + 1;
 		}else if (romanNum == "IX"){
 			return 10 - 1;
-		}else if (romanNum == "X"){
-			return convertSingleRoman(romanNum);
 		}else if (romanNum == "XX"){
 			return 10 + 10;
 		}else if (romanNum == "XXX"){
 			return 10 + 10 + 10;
 		}else if (romanNum == "XL"){
 			return 50 - 10;
-		}else if (romanNum == "L"){
-			return convertSingleRoman(romanNum);
 		}else if (romanNum == "LX"){
 			return 50 + 10;
 		}else if (romanNum == "LXX"){
@@ -40,16 +38,12 @@ public class RomanNumerals {
 			return 50 + 10 + 10 + 10;
 		}else if (romanNum == "XC"){
 			return 100 - 10;
-		}else if (romanNum == "C"){
-			return convertSingleRoman(romanNum);
 		}else if (romanNum == "CC"){
 			return 100 + 100;
 		}else if (romanNum == "CCC"){
 			return 100 + 100 + 100;
 		}else if (romanNum == "CD"){
 			return 500 - 100;
-		}else if (romanNum == "D"){
-			return convertSingleRoman(romanNum);
 		}else if (romanNum == "DC"){
 			return 500 + 100;
 		}else if (romanNum == "DCC"){
@@ -58,8 +52,6 @@ public class RomanNumerals {
 			return 500 + 100 + 100 + 100;
 		}else if (romanNum == "CM"){
 			return 1000 - 100;
-		}else if (romanNum == "M"){
-			return convertSingleRoman(romanNum);
 		}
 		return 0;
 	}
