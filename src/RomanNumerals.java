38838d8a777729ba314e99d88f4f005077818a2f
somename <someemail>
2016-09-30 16:31:20 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,45 @@
 
 public class RomanNumerals {
+
+
+	public RomanNumerals() {
+	}
+
 	public int convertToInteger(String romanNum) {
+
+		romanNum.toUpperCase();
+
+		if (romanNum.equals("I")) {
+			return 1;
+
+		} else if (romanNum == "II") {
+
+			return 2;
+		} else if (romanNum == "III") {
+
+			return 3;
+		} else if (romanNum == "IV") {
+
+			return 4;
+		} else if (romanNum == "V") {
+
+			return 5;
+		} else if (romanNum == "VI") {
+
+			return 6;
+		} else if (romanNum == "VII") {
+
+			return 7;
+		} else if (romanNum == "VIII") {
+
+			return 8;
+		} else if (romanNum == "IX") {
+
+			return 9;
+		}
+
 		// To be Implemented
 		return 0;
-		
+
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -6,7 +6,16 @@ public class TestRomanNumerals {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		
+	RomanNumerals obj = new RomanNumerals();
+	int num = obj.convertToInteger("I");
+
+	assertEquals("", 1, num);
+	
+	
+		
+		
 	}
 
 }
