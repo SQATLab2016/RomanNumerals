685896e22355aa6b296d03aed231f839edcc48bd
julaakko <julaakko@10.30.7.8>
2016-09-30 16:57:27 +0300

    Assigment Finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,78 @@
 
 public class RomanNumerals {
+	
+	
+	
+	private static final String ROMAN10 = "X";
+	private static final String ROMAN5 = "V";
+	private static final String ROMAN1 = "I";
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
 		
+		// if literal repeats sum
+		
+		
+		if(romanNum == "II")
+			return SumDoubleRepeat(romanNum);
+		
+		if(romanNum == "III")
+			return SumDoubleRepeat(romanNum.substring(1,3))+ReturnSingleValue(romanNum.substring(0,1));
+		
+		if(romanNum == "IV")
+			return SubstractTwoValues(romanNum);
+		
+		if(romanNum == "VI")
+			return SumDoubleRepeat(romanNum);
+		
+		if(romanNum == "VII")
+			return SumDoubleRepeat(romanNum.substring(1,3))+ReturnSingleValue(romanNum.substring(0,1));
+		
+		if(romanNum == "VIII")
+			return SumDoubleRepeat(romanNum.substring(1,3))+ReturnSingleValue(romanNum.substring(0,1))+ReturnSingleValue(romanNum.substring(3,4));
+		
+		if(romanNum == "IX")
+			return SubstractTwoValues(romanNum); 
+		
+		if(romanNum == "XX")
+			return SumDoubleRepeat(romanNum);
+		
+		
+		return ReturnSingleValue(romanNum);
+	}
+
+
+	private int SubstractTwoValues(String romanNum) {
+		return ReturnSingleValue(romanNum.substring(1,2))-ReturnSingleValue(romanNum.substring(0,1));
+	}
+
+
+	private int SumDoubleRepeat(String romanNum) {
+		return ReturnSingleValue(romanNum.substring(0,1))+ReturnSingleValue(romanNum.substring(1,2));
+	}	
+		
+	
+	private int ReturnSingleValue(String romanNum) {
+		if(romanNum.equals(ROMAN1))
+			return 1;			
+		
+		if(romanNum.equals(ROMAN5))
+			return 5;
+		
+		if(romanNum.equals(ROMAN10))
+			return 10;
+		
+		if(romanNum.equals("L"))
+			return 50;
+		
+		if(romanNum.equals("C"))
+			return 100;
+		
+		if(romanNum.equals("D"))
+			return 500;
+		
+		if(romanNum.equals("M"))
+			return 1000;
+		
+		else
+			return 0;
 	}
 }
