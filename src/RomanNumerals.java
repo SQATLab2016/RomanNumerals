02c5d713b958887780f224902c775270952f5b25
mikkoyli <mikkoyli@STUPID-PC722.stnet.local>
2016-09-30 16:53:31 +0300

    Project finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,54 @@
 
+
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+	public int convertToInteger(String romanNum) {		
 		
+		int length=romanNum.length();
+		int i=0;
+		int number = 0;
+		while(i<length)
+	    {
+			
+			if ((i+1)<length && valueOfLetter(romanNum.charAt(i)) < valueOfLetter(romanNum.charAt(i+1))){ //substract
+				number += valueOfLetter(romanNum.charAt(i));
+				i+=2;
+			}
+			else {
+				number += valueOfLetter(romanNum.charAt(i));
+				i++;
+			}
+	    }
+		
+		return number;
+	}
+	
+	private int valueOfLetter(Character  letter) {
+		int number = 0;
+		
+		switch (letter) {
+		case 'I':
+			number = 1;
+            break;
+        case 'V':
+        	number = 5;
+            break;
+        case 'X':
+        	number = 10;
+            break;
+        case 'L':
+        	number = 50;
+            break;
+        case 'C':
+        	number = 100;
+            break;  
+        case 'D':
+        	number = 500;
+            break;   
+        case 'M':
+        	number = 1000;
+            break;  
+		}
+		
+		return number;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,64 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	
+	RomanNumerals roman = new RomanNumerals();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_convert_I_to_1() {
+		int i = roman.convertToInteger("I");
+		assertEquals(1, i);
+	}
+	
+	@Test
+	public void test_convert_II_to_2() {
+		int i = roman.convertToInteger("II");
+		assertEquals(2, i);
+	}
+	
+	@Test
+	public void test_convert_III_to_3() {
+		int i = roman.convertToInteger("III");
+		assertEquals(3, i);
+	}
+	
+	@Test
+	public void test_convert_IV_to_4() {
+		int i = roman.convertToInteger("IV");
+		assertEquals(4, i);
+	}
+
+	@Test
+	public void test_convert_V_to_5() {
+		int i = roman.convertToInteger("V");
+		assertEquals(5, i);
+	}
+	
+	@Test
+	public void test_convert_X_to_10() {
+		int i = roman.convertToInteger("X");
+		assertEquals(10, i);
+	}
+	
+	@Test
+	public void test_convert_C_to_100() {
+		int i = roman.convertToInteger("C");
+		assertEquals(100, i);
+	}
+	
+	@Test
+	public void test_convert_D_to_500() {
+		int i = roman.convertToInteger("D");
+		assertEquals(500, i);
+	}
+	
+	@Test
+	public void test_convert_M_to_1000() {
+		int i = roman.convertToInteger("M");
+		assertEquals(1000, i);
 	}
 
 }
