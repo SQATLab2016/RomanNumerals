a0af5d308441408e9a90f5e520bd122595bb1c6b
somename <someemail>
2016-09-30 16:24:09 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,26 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		
+		int decimalNum = 0;
+		
+		// Convert romanNums to upper case, if they are lower case
+		String romanNum = romanNum.toUpperCase();
+		
+		for (int x romanNum.length() - 1; x >= 0; x--) {
+			char convertToDecimalNum = romanNum.charAt(x);
+			
+			switch (convertToDecimalNum) {
+			
+			case 'I':
+				decimalNum = 1;
+				
+				break;
+			
+			}
+		}
+		
+		return decimalNum;
 		
 	}
 }
