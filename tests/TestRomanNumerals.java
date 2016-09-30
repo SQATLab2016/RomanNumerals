3a59a0b24181eb546fbb61fb3d9ac5e98a35d0be
somename <someemail>
2016-09-30 16:33:32 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,17 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
+	public void testIs1() {
+		String number = RomanNumerals
+	}
+	
+	@Test
+	public void IXCMcanBeRepated3() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void VLDcannotBeRepeated() {
+		fail("Not yet implemented");
+	}
 }
