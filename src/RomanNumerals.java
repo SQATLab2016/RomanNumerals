e34378e577c532baa98ea77673e28a996b58bf19
ihietani <ihietani@STUPID-PC730.stnet.local>
2016-09-30 17:08:59 +0300

    Project finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,48 @@
+import java.util.*;
 
 public class RomanNumerals {
+	
+	private TreeMap<String, Integer> numbers;
+	
+	public RomanNumerals(){
+		numbers = new TreeMap<String, Integer>();
+		numbers.put("I", 1);
+		numbers.put("V", 5);
+		numbers.put("X", 10);
+		numbers.put("L", 50);
+		numbers.put("C", 100);
+		numbers.put("D", 500);
+		numbers.put("M", 1000);
+	}
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		
+		return convertMultiple(romanNum);
 		
 	}
+	private int convertMultiple(String romanNum){
+		int num = 0;
+		String[] n = romanNum.split("");
+		String prev = "";
+		for(int i = 0; i < romanNum.length(); i++){
+			if(prev.equals(n[i]) && (n[i].equals("V") || n[i].equals("L") || n[i].equals("D"))){
+				num = 0;
+				break;
+			}
+			if(prev.equals("I") && (n[i].equals("V") || n[i].equals("X"))){
+				num -= 2;
+			}
+			else if(prev.equals("X") && n[i].equals("C") || n[i].equals("D")){
+				num -= 20;
+			}
+			else if(prev.equals("C") && n[i].equals("D") || n[i].equals("M")){
+				num -= 200;
+			}
+			num += numbers.get(n[i]);
+			prev = n[i];
+			
+		}
+		return num;
+	}
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -4,9 +4,69 @@ import org.junit.Test;
 
 public class TestRomanNumerals {
 
+	RomanNumerals num = new RomanNumerals();
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_ConvertToInteger() {
+		
+		int arabic = num.convertToInteger("I");
+		assertEquals("Number is not one", 1, arabic);
 	}
+	@Test
+	public void test_ConvertToIntegerThree(){
+		
+		int arabic = num.convertToInteger("III");
+		assertEquals("Number is not three", 3, arabic);
+	}
+	@Test
+	public void test_ConvertToIntegerFive(){
+		
+		int arabic = num.convertToInteger("V");
+		assertEquals("Number is not five", 5, arabic);
+	}
+	@Test
+	public void test_ConvertToIntegerTen(){
+		
+		int arabic = num.convertToInteger("X");
+		assertEquals("Number is not ten", 10, arabic);
+	}
+	@Test
+	public void test_ConvertToIntegerFifty(){
+		
+		int arabic = num.convertToInteger("L");
+		assertEquals("Number is not fifty", 50, arabic);
+	}
+	@Test
+	public void test_ConvertToIntegerHundred(){
+		
+		int arabic = num.convertToInteger("C");
+		assertEquals("Number is not hundred", 100, arabic);
+	}
+	@Test
+	public void test_ConvertToIntegerFour(){
+		
+		int arabic = num.convertToInteger("IV");
+		assertEquals("Number is not four", 4, arabic);
+	}
+	@Test
+	public void test_ConvertToIntegerSeve(){
+		int arabic = num.convertToInteger("VII");
+		assertEquals("Number is not seven", 7, arabic);
+	}
+	@Test
+	public void test_ConvertToIntegerNine(){
+		int arabic = num.convertToInteger("IX");
+		assertEquals("Number is not nine", 9, arabic);
+	}
+	@Test
+	public void test_NotRepeated(){
+		int arabic = num.convertToInteger("VV");
+		assertEquals("Number cannot be repeated", 0, arabic);
+	}
+	@Test
+	public void test_Substraction(){
+		int arabic = num.convertToInteger("XXC");
+		assertEquals("Number is substracted even though not possible", 0, arabic);
+	}
+	
 
 }
