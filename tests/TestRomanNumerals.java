1980ad0f2a0cdac505a8d67116d7b8843b69e68a
somename <someemail>
2016-09-30 16:11:15 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,11 +5,18 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
+	public void test_I() {
 		RomanNumerals roman = new RomanNumerals();
 		int result = roman.convertToInteger("I");
 		assertEquals(1, result);
 	}
+	@Test
+	public void test_II() {
+		RomanNumerals roman = new RomanNumerals();
+		int result = roman.convertToInteger("II");
+		assertEquals(2, result);
+	}
 
+	
 	
 }
