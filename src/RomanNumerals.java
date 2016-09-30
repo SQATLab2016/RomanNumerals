ce02e0eec730c288554a86ae3ee8be9cf3b59b47
somename <someemail>
2016-09-30 16:05:58 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -8,9 +8,11 @@ public class RomanNumerals {
 		return 1;
 		}else if (romanNum.contains("II") == true){
 		return 2;
-		}else if (romanNum == "V"){
-		return 5;
-		}else if (romanNum ==)
+		}else if (romanNum.contains("III") == true){
+		return 3;
+		}else if (romanNum.contains("IV") == true){
+		return 4;
+		}
 		return 0;
 	}
 }
