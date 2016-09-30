26958483b7e3499bcb39ccab67e60d83063c59ed
somename <someemail>
2016-09-30 16:47:37 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -7,6 +7,7 @@ public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 
 		romanNum = romanNum.toUpperCase();
+		romanNum = romanNum.substring(0);
 
 		if (romanNum.equals("I")) {
 			return 1;
@@ -40,5 +41,64 @@ public class RomanNumerals {
 		// To be Implemented
 		return 0;
 
+	}
+
+	public int romanConvert(String roman) {
+
+		int decimal = 0;
+
+		String romanNumeral = roman.toUpperCase();
+		for (int x = 0; x < romanNumeral.length(); x++) {
+			char convertToDecimal = roman.charAt(x);
+
+			switch (convertToDecimal) {
+			case 'M':
+				decimal += 1000;
+				break;
+
+			case 'D':
+				decimal += 500;
+				break;
+
+			case 'C':
+				decimal += 100;
+				break;
+
+			case 'L':
+				decimal += 50;
+				break;
+
+			case 'X':
+				decimal += 10;
+				break;
+
+			case 'V':
+				decimal += 5;
+				break;
+
+			case 'I':
+				decimal += 1;
+				break;
+			}
+		}
+		if (romanNumeral.contains("IV")) {
+			decimal -= 2;
+		}
+		if (romanNumeral.contains("IX")) {
+			decimal -= 2;
+		}
+		if (romanNumeral.contains("XL")) {
+			decimal -= 10;
+		}
+		if (romanNumeral.contains("XC")) {
+			decimal -= 10;
+		}
+		if (romanNumeral.contains("CD")) {
+			decimal -= 100;
+		}
+		if (romanNumeral.contains("CM")) {
+			decimal -= 100;
+		}
+		return decimal;
 	}
 }
