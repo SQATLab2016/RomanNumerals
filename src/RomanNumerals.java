d7ec5621551a58a9f06af286932a426947f946a7
somename <someemail>
2016-09-30 15:47:38 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -5,7 +5,12 @@ public class RomanNumerals {
 	
 	public int convertToInteger(String romanNum) {
 		if (romanNum == "") return 0;
-		if (romanNum.startsWith("M")) return 1000 + convertToInteger(romanNum.)
+		if (romanNum.startsWith("M")) return 1000 + convertToInteger(romanNum.replace("M", ""));
+		if (romanNum.startsWith("CM")) return 900 + convertToInteger(romanNum.replace("CM", ""));
+		if (romanNum.startsWith("C")) return 500 + convertToInteger(romanNum.replace("M", ""));
+		if (romanNum.startsWith("CD")) return 400 + convertToInteger(romanNum.replace("M", ""));
+		if (romanNum.startsWith("C")) return 100 + convertToInteger(romanNum.replace("M", ""));
+		if (romanNum.startsWith("CX")) return 1000 + convertToInteger(romanNum.replace("M", ""));
 		return 0;
 		
 	}
