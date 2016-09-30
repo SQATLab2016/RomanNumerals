0711759ce376062ec72b247dd6b03c3eaeb5d866
dbb12 <397628929@qq.com>
2016-09-30 16:50:23 +0300

    Update RomanNumerals.java

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,7 +1,14 @@
 import java.util.HashMap; 
-
 public class RomanNumerals {
-	public static int convertToInteger(String romanNum) {
+	public int convertToInteger(String romanNum) throws Exception {
+		String[] str = new String[6];
+		str[0] = "VV";	str[0] = "LL";	str[0] = "DD";	str[0] = "IIII";	str[0] = "XXXX";	str[0] = "CCCC";
+		int i=0;
+		for( i=0 ; i<6 ; i++)
+		{
+			if( romanNum.contains(str[i]))
+				throw new ContainSuccessive_1_Number();
+		}
 		HashMap<String,Integer> hashmap = new HashMap<String,Integer>();
 		hashmap.put("I", 1);	hashmap.put("V", 5);	hashmap.put("X", 10);	hashmap.put("L", 50);
 		hashmap.put("C", 100);	hashmap.put("D", 500);	hashmap.put("M", 1000);	hashmap.put("IV", 4);
@@ -10,14 +17,22 @@ public class RomanNumerals {
 		int index = 0;
 		while( index<romanNum.length() )
 		{
+			if( !hashmap.containsKey(romanNum.substring(index,index+1)) )
+				throw new ContainOtherCharacterException();
 			if( index==romanNum.length()-1 )
-			{		result +=hashmap.get(romanNum.substring(index,romanNum.length()));		break;	}
+			{		result += hashmap.get(romanNum.substring(index,romanNum.length()));		break;	}
 			if( hashmap.containsKey(romanNum.substring(index,index+2)) )
 			{		result += hashmap.get(romanNum.subSequence(index, index+2));	index +=2;		}
 			else
-			{		result += hashmap.get(romanNum.substring(index,index+1));		index++;		}
+			{	result += hashmap.get(romanNum.substring(index,index+1));		index++;		}
 		}
-		return result;
-		
+		return result;		
+	}
+	class ContainOtherCharacterException extends Exception{
+		public ContainOtherCharacterException(){}
+	}
+	
+	class ContainSuccessive_1_Number extends Exception{
+		public ContainSuccessive_1_Number(){}
 	}
 }
