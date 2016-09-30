91c6c47fbc4e24675867b0d53af48b98c8a932a9
somename <someemail>
2016-09-30 16:01:36 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -6,11 +6,11 @@ public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		if (romanNum == "I"){
 		return 1;
-		}else if (romanNum == "IV"){
+		}else if (romanNum.contains("IV") == true){
 		return 4;
 		}else if (romanNum == "V"){
 		return 5;
-		}
+		}else if (romanNum ==)
 		return 0;
 	}
 }
