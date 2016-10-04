55f45c43427a4855342b617b563f001630502fd6
usern12 <>
2016-10-04 08:09:36 +0300

    Project finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,66 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+	
+	private int totalArabic;
+	private int lastNumber;
+	
+	public RomanNumerals() {
+        this.totalArabic = 0;
+        this.lastNumber = 0;
 	}
+	
+	public int convertToInteger(String romanNum) {	
+        for (int i = romanNum.length() - 1; i >= 0 ; i--) {
+            handleRomanNumbers(romanNum, i);
+        }
+       return totalArabic;
+    }
+
+	private void handleRomanNumbers(String romanNum, int i) {
+		switch (romanNum.charAt(i)) {
+		    case 'M':
+		        processtotalArabic(1000);
+		        lastNumber = 1000;
+		        break;
+
+		    case 'D':
+		    	processtotalArabic(500);
+		        lastNumber = 500;
+		        break;
+
+		    case 'C':
+		    	processtotalArabic(100);
+		        lastNumber = 100;
+		        break;
+
+		    case 'L':
+		    	processtotalArabic(50);
+		        lastNumber = 50;
+		        break;
+
+		    case 'X':
+		    	processtotalArabic(10);
+		        lastNumber = 10;
+		        break;
+
+		    case 'V':
+		    	processtotalArabic(5);
+		        lastNumber = 5;
+		        break;
+
+		    case 'I':
+		    	processtotalArabic(1);
+		        lastNumber = 1;
+		        break;
+		}
+	}
+
+    private void processtotalArabic(int arabic) {
+        if (lastNumber > arabic) {
+           totalArabic -= arabic;
+        } else {
+          totalArabic += arabic;
+        }
+    }
 }
+
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -4,9 +4,61 @@ import org.junit.Test;
 
 public class TestRomanNumerals {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	RomanNumerals romanNumerals = new RomanNumerals();
+	private void assertRomanNumerals(String roman, int expected) {
+		
+		assertEquals(expected, romanNumerals.convertToInteger(roman));
+	}
+	@Test public void testRomanNumerals_I_1() {
+		assertRomanNumerals ("I", 1);
 	}
 
+	@Test public void testRomanNumerals_V_5() {
+		assertRomanNumerals ("V", 5);
+	}
+	
+	@Test public void testRomanNumerals_X_10() {
+		assertRomanNumerals ("X", 10);
+	}
+	
+	@Test public void testRomanNumerals_L_50() {
+		assertRomanNumerals ("L", 50);
+	}
+	
+	@Test public void testRomanNumerals_C_100() {
+		assertRomanNumerals ("C", 100);
+	}
+	
+	@Test public void testRomanNumerals_D_500() {
+		assertRomanNumerals ("D", 500);
+	}
+	
+	@Test public void testRomanNumerals_M_1000() {
+		assertRomanNumerals ("M", 1000);
+	}
+	
+	@Test public void testRomanNumerals_II_2() {
+		assertRomanNumerals ("II", 2);
+	}
+	
+	@Test public void testRomanNumerals_IV_4() {
+		assertRomanNumerals("IV", 4);
+	}
+	
+	@Test public void testRomanNumerals_IX_9() {
+		assertRomanNumerals("IX", 9);
+	}
+	
+	@Test public void testRomanNumerals_LXXX_80() {
+		assertRomanNumerals("LXXX", 80);
+	}
+	
+	@Test public void testRomanNumerals_MCMLXXXIV_1984() {
+		assertRomanNumerals("MCMLXXXIV", 1984);
+	}
+	
+	@Test public void testRomanNumerals_MMXIV_2014() {
+		assertRomanNumerals("MMXIV", 2014);
+	}
+	 
 }
