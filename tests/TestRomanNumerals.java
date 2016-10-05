53742c0a7b2d6993001068ef2c21bb54c846add8
somename <someemail>
2016-10-05 15:47:59 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -135,7 +135,6 @@ public class TestRomanNumerals {
 		assertEquals(43, num);
 	}
 	
-	/*
 	@Test public void testRomanNumerals_1_to_3999() throws Exception  {
 		RomanNumeral testValue;
 		for (int i = 1; i < 4000; i++) {
@@ -144,6 +143,6 @@ public class TestRomanNumerals {
 			int converted = rn.convertToInteger(testValue.toString());
 			assertEquals(testValue.toInt(), converted);
 		}
-	}*/
+	}
 
 }
