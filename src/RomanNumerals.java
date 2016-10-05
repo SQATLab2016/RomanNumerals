61f98dc5a0f1e43c38a6852c907c28f6c45a73c0
somename <someemail>
2016-10-05 14:24:52 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -20,7 +20,7 @@ public class RomanNumerals {
 				+ "VX|VL|VC|VD|VM|LC|LD|LM|DM|"
 				
 				// other than the allowed symbols
-				+ "\\d"
+				+ "[^IVXLCDM]"
 				);
 		Matcher m = p.matcher(romanNum);
 		if (m.find()) {
