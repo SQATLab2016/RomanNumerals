13b1fd1c0879b250860373fd63937300ff3a900a
somename <someemail>
2016-10-05 15:01:17 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,7 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RomanNumerals {
+	private enum Numeral {
+		I(1),
+		V(5),
+		X(10),
+		L(50),
+		C(100),
+		D(500),
+		M(1000);
+		
+		private int number;
+		
+		private Numeral(int number) {
+			this.number = number;
+		}
+		
+		public int value() {
+			return this.number;
+		}
+	}
+	
 	public int convertToInteger(String romanNum) {
 		String patternString = String.join("|",
 				// repeat limit
@@ -33,16 +55,33 @@ public class RomanNumerals {
 			throw new IllegalArgumentException();
 		}
 		
-		if (romanNum.equals("I")) {
-			return 1;
-		} else if (romanNum.equals("II")) {
-			return 2;
-		} else if (romanNum.equals("III")) {
+		for (int i = 0; i < romanNum.length(); i++) {
+			String numeral = String.valueOf(romanNum.charAt(i));
+			int number = Numeral.valueOf(numeral).value();
+			
+			// last numeral
+			if (i + 1 == romanNum.length()) {
+				return number;
+			}
+			
+			String nextNumeral = String.valueOf(romanNum.charAt(i + 1));
+			int nextNumber = Numeral.valueOf(nextNumeral).value();
+
+			// subtract or add
+			if (number < nextNumber) {
+				return nextNumber - number;
+			} else if (number > nextNumber) {
+				return number + nextNumber;
+			}
+			
+			// second last numeral
+			if (i + 2 == romanNum.length()) {
+				if (number == nextNumber) {
+					return 2;
+				}
+			}
+			
 			return 3;
-		} else if (romanNum.equals("IV")) {
-			return 4;
-		} else {
-			return 6;
 		}
 	}
 }
