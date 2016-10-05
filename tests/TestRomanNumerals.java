d7654477a10af54fd810bcdba29c00ed70b1956f
somename <someemail>
2016-10-05 15:16:33 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -15,7 +15,7 @@ public class TestRomanNumerals {
 	
 	@Test
 	public void repetitionCharTest() {
-		RomanNumerals rm = new RomanNumerals("XXXMMMVIIIV");
+		RomanNumerals rm = new RomanNumerals("XXXMMMDIIIV");
 		Boolean valid = false;
 		valid = rm.repetitionCharCheck(); 
 		
