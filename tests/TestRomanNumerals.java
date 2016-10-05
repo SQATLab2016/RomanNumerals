328f35653b7c2587ed831b3bf7a98bb231a80c41
somename <someemail>
2016-10-05 15:39:11 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -6,41 +6,6 @@ import org.junit.Test;
 
 public class TestRomanNumerals {
 	
-    private static int decodeSingle(char letter) {
-        switch (letter) {
-            case 'M':
-                return 1000;
-            case 'D':
-                return 500;
-            case 'C':
-                return 100;
-            case 'L':
-                return 50;
-            case 'X':
-                return 10;
-            case 'V':
-                return 5;
-            case 'I':
-                return 1;
-            default:
-                return 0;
-        }
-    }
-
-    public static int decode(String roman) {
-        int result = 0;
-        String uRoman = roman.toUpperCase(); //case-insensitive
-        for (int i = 0; i < uRoman.length() - 1; i++) {//loop over all but the last character
-            if (decodeSingle(uRoman.charAt(i)) < decodeSingle(uRoman.charAt(i + 1))) {
-                result -= decodeSingle(uRoman.charAt(i));
-            } else {
-                result += decodeSingle(uRoman.charAt(i));
-            }
-        }
-        result += decodeSingle(uRoman.charAt(uRoman.length() - 1));
-        return result;
-    }
-	
 	private RomanNumerals rn = new RomanNumerals();
 
 	@Test public void testRomanNumerals_emptyString() throws Exception {
@@ -167,7 +132,6 @@ public class TestRomanNumerals {
 	
 	@Test public void testRomanNumerals_2014_2014() throws Exception  {
 		int testValue = 2014;
-		String roman = decode(2014);
 		System.err.println(roman);
 		int converted = rn.convertToInteger(roman);
 		assertEquals(testValue, converted);
