8d98efad59d6e913055c9f3435aa52494c9af298
somename <someemail>
2016-10-05 14:29:47 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -6,7 +6,7 @@ public class RomanNumerals {
 		int loop = 0;
 
 		validateRomanString(rommanNumArray);
-		
+
 		while (loop < rommanNumArray.length) {
 			if (loop + 1 < rommanNumArray.length
 					&& checkIfFirstIsSmall(rommanNumArray[loop], rommanNumArray[loop + 1])) {
@@ -21,8 +21,35 @@ public class RomanNumerals {
 		return result;
 	}
 
-	private void validateRomanString(char[] rommanNumArray) throws RomanNumeralException{
-		
+	private void validateRomanString(char[] rommanNumArray) throws RomanNumeralException {
+		int countI = 0;
+		int countX = 0;
+		int countM = 0;
+		int countC = 0;
+		int countV = 0;
+		int countL = 0;
+		int countD = 0;
+
+		for (int loop = 0; loop < rommanNumArray.length; loop++) {
+			if (rommanNumArray[loop] == 'V')
+				countV++;
+			else if (rommanNumArray[loop] == 'X')
+				countX++;
+			else if (rommanNumArray[loop] == 'I')
+				countI++;
+			else if (rommanNumArray[loop] == 'M')
+				countM++;
+			else if (rommanNumArray[loop] == 'C')
+				countC++;
+			else if (rommanNumArray[loop] == 'L')
+				countL++;
+			else if (rommanNumArray[loop] == 'D')
+				countD++;
+		}
+
+		if (countI > 3 || countX > 3 || countM > 3 || countC > 3 || countV > 1 || countL > 1 || countD > 1) {
+			throw new RomanNumeralException();
+		}
 	}
 
 	private boolean checkIfFirstIsSmall(char c, char d) {
