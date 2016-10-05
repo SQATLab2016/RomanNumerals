823b7de6b898b587e97ea399d610694e753e1f05
somename <someemail>
2016-10-05 14:55:11 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,35 @@
 
 public class RomanNumerals {
+	
+	private String number;
+
+	public RomanNumerals(String string) {
+		number = string;
+	}
+
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
 		return 0;
 		
 	}
+
+	public Boolean atMost3CharCheck(char ch) {
+		int counter = 0;
+		
+		for(int i = 0; i<number.length(); ++i) {
+			
+			if(ch == number.charAt(i)) {
+				++counter;
+			}
+			
+		}
+		
+		if(counter>3) {
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
 }
