07ca3095dd3f9cc37000047d7be1f0abdfbf8eda
somename <someemail>
2016-10-05 14:43:50 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
+	public void atMost3CharTest() {
+		RomanNumerals rm = new RomanNumerals("MMMMXIV");
+		Boolean valid = atMost3CharCheck("M"); 
+		
 		fail("Not yet implemented");
 	}
-
+	
+	
+	
 }
