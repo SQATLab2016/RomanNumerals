be9ae99b117a3bf6b0ac28439fcebbe7e44974b6
somename <someemail>
2016-10-05 15:08:57 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -6,7 +6,7 @@ public class TestRomanNumerals {
 
 	@Test
 	public void atMost3CharTest() {
-		RomanNumerals rm = new RomanNumerals("XXXMMMIV");
+		RomanNumerals rm = new RomanNumerals("XXXMMMXIV");
 		Boolean valid = false;
 		valid = rm.atMost3CharCheck(); 
 		
