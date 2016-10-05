5c5642333463bd1af6f55bb4e8ba90f5f7195b7f
somename <someemail>
2016-10-05 14:32:50 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -11,61 +11,66 @@ public class TestRomanNumerals {
 	}
 
 	@Test
-	public void testRomanNumeral_I_1() {
+	public void testRomanNumeral_I_1() throws RomanNumeralException {
 		assertRomanNumerals(1, "I");
 	}
 
 	@Test
-	public void testRomanNumeral_V_5() {
+	public void testRomanNumeral_V_5() throws RomanNumeralException {
 		assertRomanNumerals(5, "V");
 	}
 
 	@Test
-	public void testRomanNumeral_X_10() {
+	public void testRomanNumeral_X_10() throws RomanNumeralException {
 		assertRomanNumerals(10, "X");
 	}
 
 	@Test
-	public void testRomanNumeral_L_50() {
+	public void testRomanNumeral_L_50() throws RomanNumeralException {
 		assertRomanNumerals(50, "L");
 	}
 
 	@Test
-	public void testRomanNumeral_C_100() {
+	public void testRomanNumeral_C_100() throws RomanNumeralException {
 		assertRomanNumerals(100, "C");
 	}
 
 	@Test
-	public void testRomanNumeral_D_500() {
+	public void testRomanNumeral_D_500() throws RomanNumeralException {
 		assertRomanNumerals(500, "D");
 	}
 
 	@Test
-	public void testRomanNumeral_M_1000() {
+	public void testRomanNumeral_M_1000() throws RomanNumeralException {
 		assertRomanNumerals(1000, "M");
 	}
 
 	@Test
-	public void testRomanNumeral_IV_4() {
+	public void testRomanNumeral_IV_4() throws RomanNumeralException {
 		assertRomanNumerals(4, "IV");
 	}
 
 	@Test
-	public void testRomanNumeral_MCMLXXXIV_1984() {
+	public void testRomanNumeral_MCMLXXXIV_1984() throws RomanNumeralException {
 		assertRomanNumerals(1984, "MCMLXXXIV");
 	}
 
 	@Test
-	public void testRomanNumeral_MMXIV_2014() {
+	public void testRomanNumeral_MMXIV_2014() throws RomanNumeralException {
 		assertRomanNumerals(1984, "MCMLXXXIV");
 	}
 	
 	@Test (expected = RomanNumeralException.class)
-	public void RomanNumeral_InvalidInputCheckException() {
-		int result = romanNumerals.convertToInteger("MMMM");
+	public void RomanNumeral_InvalidInputWith4M() throws RomanNumeralException {
+		romanNumerals.convertToInteger("MMMM");
 	}
 
-	public void assertRomanNumerals(int expectedOutput, String input) {
+	@Test (expected = RomanNumeralException.class)
+	public void RomanNumeral_InvalidInputWith2V() throws RomanNumeralException {
+		romanNumerals.convertToInteger("VV");
+	}
+	
+	public void assertRomanNumerals(int expectedOutput, String input) throws RomanNumeralException {
 		int result = romanNumerals.convertToInteger(input);
 		assertEquals(expectedOutput, result);
 	}
