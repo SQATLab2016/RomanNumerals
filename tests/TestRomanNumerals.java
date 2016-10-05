33bc83c2e169bf129bc830ddec0c2aa53467c59d
somename <someemail>
2016-10-05 14:09:46 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -4,54 +4,60 @@ import org.junit.Test;
 
 public class TestRomanNumerals {
 	RomanNumerals romanNumerals = new RomanNumerals();
+
 	@Test
-	public void testCheckRomanNumerals_null() {		
+	public void testCheckRomanNumerals_null() {
 		assertNotNull(romanNumerals);
 	}
 
 	@Test
-	public void testRomanNumeral_I_1(){
+	public void testRomanNumeral_I_1() {
 		assertRomanNumerals(1, "I");
 	}
-	
+
 	@Test
-	public void testRomanNumeral_V_5(){
+	public void testRomanNumeral_V_5() {
 		assertRomanNumerals(5, "V");
 	}
-	
+
 	@Test
-	public void testRomanNumeral_X_10(){
+	public void testRomanNumeral_X_10() {
 		assertRomanNumerals(10, "X");
 	}
-	
+
 	@Test
-	public void testRomanNumeral_L_50(){
+	public void testRomanNumeral_L_50() {
 		assertRomanNumerals(50, "L");
 	}
-	
+
 	@Test
-	public void testRomanNumeral_C_100(){
+	public void testRomanNumeral_C_100() {
 		assertRomanNumerals(100, "C");
 	}
-	
+
 	@Test
-	public void testRomanNumeral_D_500(){
+	public void testRomanNumeral_D_500() {
 		assertRomanNumerals(500, "D");
 	}
-	
+
 	@Test
-	public void testRomanNumeral_M_1000(){
+	public void testRomanNumeral_M_1000() {
 		assertRomanNumerals(1000, "M");
 	}
-	
+
 	@Test
-	public void testRomanNumeral_IV_4(){
+	public void testRomanNumeral_IV_4() {
 		assertRomanNumerals(4, "IV");
 	}
-	
-	public void assertRomanNumerals(int expectedOutput, String input){
+
+	@Test
+	public void testRomanNumeral_MCMLXXXIV_1984() {
+		assertRomanNumerals(1984, "MCMLXXXIV");
+	}
+
+	public void assertRomanNumerals(int expectedOutput, String input) {
 		int result = romanNumerals.convertToInteger(input);
 		assertEquals(expectedOutput, result);
 	}
-	
+
 }
