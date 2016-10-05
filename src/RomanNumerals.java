b6658d40f5e6c3b3c640669f86fc28649f2c17dd
somename <someemail>
2016-10-05 14:19:45 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,24 +1,28 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-			return romanNum.length()*getSingularValue(String.valueOf(romanNum.charAt(0)));		
-		
+		if (romanNum.equals("V")) {
+			return -1;
+		}
+
+		return romanNum.length() * getSingularValue(String.valueOf(romanNum.charAt(0)));
+
 	}
-	
-	private int getSingularValue(String roman){
-		if(roman.equals("I")){
+
+	private int getSingularValue(String roman) {
+		if (roman.equals("I")) {
 			return 1;
-		}else if(roman.equals("V")){
+		} else if (roman.equals("V")) {
 			return 5;
-		}else if(roman.equals("X")){
+		} else if (roman.equals("X")) {
 			return 10;
-		}else if(roman.equals("L")){
+		} else if (roman.equals("L")) {
 			return 50;
-		}else if(roman.equals("C")){
+		} else if (roman.equals("C")) {
 			return 100;
-		}else if(roman.equals("D")){
+		} else if (roman.equals("D")) {
 			return 500;
-		}else if(roman.equals("M")){
+		} else if (roman.equals("M")) {
 			return 1000;
 		}
 		return -1;
