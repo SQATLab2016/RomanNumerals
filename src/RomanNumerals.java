0efbe03712138bc9c28679ffe68aa5efb6d66117
somename <someemail>
2016-10-05 14:38:55 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -7,17 +7,13 @@ public class RomanNumerals {
 	private static final char C = 'C';
 	private static final char D = 'D';
 	private static final char M = 'M';
-	
-	private int[] occurencies;
 
 	public int convertToInteger(String romanNum) {
-		occurencies = new int[] {0,0,0,0,0,0,0};
 		int value = 0;
 		
 		for (int i = romanNum.length() - 1; i >= 0; i--) {
 			value += parseCharacterToValue(romanNum, i);
 		}
-		System.out.println(occurencies[0]);
 		return value;
 	}
 
@@ -48,31 +44,24 @@ public class RomanNumerals {
 	
 	private int getCharValue(char romanNum) {
 		if (romanNum == I) {
-			occurencies[0]++;
 			return 1;
 		}
 		else if (romanNum == V) {
-			occurencies[1]++;
 			return 5;
 		}
 		else if (romanNum == X) {
-			occurencies[2]++;
 			return 10;
 		}
 		else if (romanNum == L) {
-			occurencies[3]++;
 			return 50;
 		}
 		else if (romanNum == C) {
-			occurencies[4]++;
 			return 100;
 		}
 		else if (romanNum == D) {
-			occurencies[5]++;
 			return 500;
 		}
 		else if (romanNum == M) {
-			occurencies[6]++;
 			return 1000;
 		}
 		return 0;
