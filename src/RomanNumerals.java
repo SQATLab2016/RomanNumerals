f409a885bd07c8ac722c95cca4d965ff6b0670f1
somename <someemail>
2016-10-05 13:57:16 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,19 +1,31 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		if(romanNum.equals("V"))
+		char[] rommanNumArray = romanNum.toCharArray();
+		int result = 0;
+		int loop = 0;
+
+		while (loop < rommanNumArray.length) {
+			result = result + checkSingleNumeral(rommanNumArray[loop]);
+		}
+
+		return result;
+	}
+
+	public int checkSingleNumeral(char numberal) {
+		if (numberal == 'V')
 			return 5;
-		else if (romanNum.equals("X"))
+		else if (numberal == 'X')
 			return 10;
-		else if (romanNum.equals("L"))
+		else if (numberal == 'L')
 			return 50;
-		else if (romanNum.equals("C"))
+		else if (numberal == 'C')
 			return 100;
-		else if (romanNum.equals("D"))
+		else if (numberal == 'D')
 			return 500;
-		else if (romanNum.equals("M"))
+		else if (numberal == 'M')
 			return 1000;
-		
-		return 1;
+		else
+			return 1;
 	}
 }
