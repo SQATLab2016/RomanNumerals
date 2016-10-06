cd5eec6f9b3b7191dd7fe87682bf309db50a02cf
somename <someemail>
2016-10-06 15:56:10 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -39,7 +39,7 @@ public class RomanNumerals {
 				c += counter-1;
 			counter = 1;
 		}
-		System.out.println(subStrings.toString());
+		//System.out.println(subStrings.toString());
 		
 		for(String s: subStrings) {
 			switch(s) {
