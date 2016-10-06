a7e8c668c815502ab906050e897f6c77f13c5799
somename <someemail>
2016-10-06 15:13:04 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -7,7 +7,7 @@ public class RomanNumerals {
 		char tempChar;
 		if (checkFor3C(romanNum)&&checkFor3I(romanNum)&&checkFor3M(romanNum)&&checkFor3X(romanNum)) {
 			if (checkForNorepeatD(romanNum)&&checkForNorepeatL(romanNum)&&checkForNorepeatV(romanNum)) {
-				for (int i = romanNum.length(); i < 1; i--) {
+				for (int i = romanNum.length(); i<0 ; i--) {
 					System.out.println("iep");
 
 					tempChar= romanNum.charAt(i);
@@ -22,7 +22,9 @@ public class RomanNumerals {
 				}
 			}
 		}
-		return 0;
+		return num;
+			
+		
 	}
 
 	public int convertCharToNum(Character a){
