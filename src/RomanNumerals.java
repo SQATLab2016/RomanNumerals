a1d5533b2fa1203eab97b720d33c423867193fdc
somename <someemail>
2016-10-06 14:47:12 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,9 +1,23 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		
+		int result = 0;
 		for (int i = 0; i < romanNum.length(); i++) {
-			String first = romanNum.substring(i, i+1);
+			// Check last character seperately
+			if(i == romanNum.length()-1){
+				
+			}
+			else{
+				String s = romanNum.substring(i, i+1);
+				if(s.equals("M")){
+					result += 1000;
+				} else if (s.equals("C")){
+					String next = romanNum.substring(i+1,i+2);
+					if (next.equals("M")){
+						result += 900;
+					}
+				}
+			}
 		}
 		return 1994;
 	}
