b064ce894144445aa5e6227e2c0bcf99eb627661
mikkoyli <mikkoyli@10.30.7.22>
2016-10-07 15:29:37 +0300

    Project finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,16 +1,35 @@
 
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {		
+	public int convertToInteger(String romanNum) throws RomanNumeralsException {		
 		
 		int length=romanNum.length();
 		int i=0;
 		int number = 0;
+		
+		if (romanNum.indexOf("IIII") >= 0)
+			throw new RomanNumeralsException("More than three I's in row is not accepted.");
+		else if (romanNum.indexOf("XXXX") >= 0)
+			throw new RomanNumeralsException("More than three X's in row is not accepted.");
+		else if (romanNum.indexOf("CCCC") >= 0)
+			throw new RomanNumeralsException("More than three C's in row is not accepted.");
+		else if (romanNum.indexOf("MMMM") >= 0)
+			throw new RomanNumeralsException("More than three M's in row is not accepted.");
+		
+		if (romanNum.indexOf("VV") >= 0)
+			throw new RomanNumeralsException("More than two V's in row is not accepted.");
+		else if (romanNum.indexOf("LL") >= 0)
+			throw new RomanNumeralsException("More than two L's in row is not accepted.");
+		else if (romanNum.indexOf("DD") >= 0)
+			throw new RomanNumeralsException("More than two D's in row is not accepted.");
+		
+		// todo: two or more smaller before big results exception
+		
 		while(i<length)
 	    {
 			
-			if ((i+1)<length && valueOfLetter(romanNum.charAt(i)) < valueOfLetter(romanNum.charAt(i+1))){ //substract
-				number += valueOfLetter(romanNum.charAt(i));
+			if ((i+1)<length && valueOfLetter(romanNum.charAt(i)) < valueOfLetter(romanNum.charAt(i+1))){
+				number += valueOfLetter(romanNum.charAt(i+1)) - valueOfLetter(romanNum.charAt(i));
 				i+=2;
 			}
 			else {
