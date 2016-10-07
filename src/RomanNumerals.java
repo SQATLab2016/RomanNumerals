2f69d163047f199815132622a70423d178ccfe39
ihietani <Ilkka.Hietaniemi@student.oulu.fi>
2016-10-07 17:06:22 +0300

    Update RomanNumerals.java

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,19 +2,17 @@ import java.util.*;
 
 public class RomanNumerals {
 	
-	final String ONE = "I", FIVE = "V", TEN = "X", FIFTY = "L", HUNDRED = "C", FIVEHUNDRED= "D", THOUSAND = "M";
-	//Trying out treeMap
 	private TreeMap<String, Integer> numbers;
 	
 	public RomanNumerals(){
 		numbers = new TreeMap<String, Integer>();
-		numbers.put(ONE, 1);
-		numbers.put(FIVE, 5);
-		numbers.put(TEN, 10);
-		numbers.put(FIFTY, 50);
-		numbers.put(HUNDRED, 100);
-		numbers.put(FIVEHUNDRED, 500);
-		numbers.put(THOUSAND, 1000);
+		numbers.put("I", 1);
+		numbers.put("V", 5);
+		numbers.put("X", 10);
+		numbers.put("L", 50);
+		numbers.put("C", 100);
+		numbers.put("D", 500);
+		numbers.put("M", 1000);
 	}
 	
 	public int convertToInteger(String romanNum) {
@@ -22,84 +20,90 @@ public class RomanNumerals {
 		return convertMultiple(romanNum);
 		
 	}
-	//If same number repeats
-	private boolean checkRepetition(String prev, String current){
-		boolean repeat = false;
-		if(prev != null && prev.equals(current)){
-			repeat = true;
-		}
-		else{
-			repeat = false;
-		}
-		return repeat;
-	}
-	//If smaller
-	private boolean checkIfSmallerNumber(String prev, String current){
-		boolean ok = false;
-		if(prev != null){
-			if(current.equals(ONE)){
-				if(prev.equals(FIVE) || prev.equals(TEN)){
-					ok = true;
-				}
-			}
-			else if(current.equals(TEN)){
-				if(prev.equals(FIFTY) || prev.equals(HUNDRED)){
-					ok = true;
-				}
-			}
-			else if(current.equals(HUNDRED)){
-				if(prev.equals(FIVEHUNDRED) || prev.equals(THOUSAND)){
-					ok = true;
-				}
-			}
-		}
-		return ok;
-	}
 	private int convertMultiple(String romanNum){
-		int num = 0;
-		
 		String[] n = romanNum.split("");
-		String prev = null;
-		int times = 0;
-		boolean substract = false;
+		if(checkThreeTimes(n) && substractOkay(n)){
+			return count(n);
+		}
+		return 0;
 		
-		for(int i = n.length - 1; i >= 0; i--){
-			
-			if(n[i].equals(FIVE) || n[i].equals(FIFTY) || n[i].equals(FIVEHUNDRED)){
-				if(checkRepetition(prev, n[i])){
-					num = 0;
-					break;
-				}
+	}
+	//count for checking the total
+	private int count(String[] n){
+		int c = numbers.get(n[n.length - 1]);
+		String prev = n[n.length - 1];
+		for(int i = n.length -2; i >= 0; i--){
+			if(numbers.get(prev) > numbers.get(n[i])){
+				c -= numbers.get(n[i]);
 			}
 			else{
-				if(checkRepetition(prev, n[i])){
-					times++;
-					if(times == 3){
-						break;
-					}
-				}
+				c += numbers.get(n[i]);
 			}
-			
-			
-			if(prev != null && numbers.get(prev) > numbers.get(n[i])){
-				if(checkIfSmallerNumber(prev, n[i])){
-					num -= numbers.get(n[i]);
-					
-				}
-				substract = true;
-				
-			}
-			else{
-				num += numbers.get(n[i]);
-			}
-			if(substract && checkRepetition(prev, n[i])){
-				num = 0;
-				break;
-			}
-			
 			prev = n[i];
 		}
-		return num;
+		return c;
+	}
+	private boolean substractOkay(String[] n){
+		String prev = n[n.length - 1];
+		int times = 0;
+		for(int i = n.length - 2; i >= 0; i--){
+			if(prev.equals("M") || prev.equals("D")){
+				if(!n[i].equals("C")){
+					return false;
+				}
+				else{
+					times++;
+				}
+			}
+			if(prev.equals("C") || prev.equals("L")){
+				if(!n[i].equals("X")){
+					return false;
+				}
+				else{
+					times++;
+				}
+			}
+			if(prev.equals("X") || prev.equals("V")){
+				if(!n[i].equals("I")){
+					return false;
+				}
+				else{
+					times++;
+				}
+			}
+			if(n[i].equals("V") || n[i].equals("L") || n[i].equals("D")){
+				if(numbers.get(n[i]) < numbers.get(prev)){
+					return false;
+				}
+			}
+			if(!n[i].equals("I") || !n[i].equals("X") || !n[i].equals("C")){
+				times = 0;
+			}
+			if(times > 1){
+				return false;
+			}
+			prev = n[i];
+		}
+		return true;
+	}
+	private boolean checkThreeTimes(String[] n){
+		
+		String prev = n[n.length - 1];
+		int times = 0;
+		for(int i = n.length - 2; i >= 0; i--){
+			if(prev.equals(n[i])){
+				times++;
+			}
+			if(times > 2){
+				return false;
+			}
+			if(!prev.equals(n[i])){
+				times = 0;
+			}
+			prev = n[i];
+		}
+		return true;
+		
 	}
 	
 }
