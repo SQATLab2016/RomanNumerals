f97b4ff9b50d8af20a939745039daa34751de297
somename <someemail>
2016-10-07 15:43:36 +0300

    besouro automatic message

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -99,7 +99,7 @@ public class TestRomanNumerals {
 	}
 	public void testRomanNumberals_Fifty() {
 		
-	int integer = test.convertToInteger("XXV");
+	int integer = test.convertToInteger("L");
 	assertEquals(25, integer);
 		
 	}
