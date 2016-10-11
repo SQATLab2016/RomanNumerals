0f2bfe1fa75353d9b17568c33538929efef3df56
somename <someemail>
2016-10-11 23:04:39 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,9 +1,30 @@
+import java.util.Hashtable;
 
 public class RomanNumerals {
-	private Hashtable<String, int> numTable;  
+	private Hashtable<String, Integer> numTable;  
 	
 	public RomanNumerals() {
-		
+		numTable = new Hashtable<String, Integer>();
+		fillNumTable();
+	}
+	
+	private void fillNumTable() {
+		numTable.put(arg0, arg1);
+		numTable.put(arg0, arg1);
+		numTable.put(arg0, arg1);
+		numTable.put(arg0, arg1);
+		numTable.put(arg0, arg1);
+		numTable.put(arg0, arg1);
+/*	1 I 	10 X 	100 C
+	2 II 	20 XX 	200 CC
+	3 III 	30 XXX 	300 CCC
+	4 IV 	40 XL 	400 CD
+	5 V 	50 L 	500 D
+	6 VI 	60 LX 	600 DC
+	7 VII 	70 LXX 	700 DCC
+	8 VIII 	80 LXXX 800 DCCC
+	9 IX 	90 XC 	900 CM
+*/
 	}
 	
 	public int convertToInteger(String romanNum) throws RomanNumeralException {
