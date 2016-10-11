0d7db62cb263821df505a7970b51f0fe6561aba4
somename <someemail>
2016-10-12 01:00:12 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -79,6 +79,7 @@ public class RomanNumerals {
 		String prev = "";
 		
 		for (int i = 0; i < num.length(); i++) {
+			System.out.println("handling " + num.substring(i, i + 1));
 			if (prev.equals(num.substring(i, i + 1)) && isOneSymbol(num.substring(i, i + 1))) {
 				subsequentAmount++;
 			} else {
@@ -126,9 +127,9 @@ public class RomanNumerals {
 		int subsequentAmount = 0;
 		
 		for (int i = 0; i < num.length(); i++) {
-			System.out.println("handling " + num.substring(i, i + 1));
+			
 			if (isFiveSymbol(num.substring(i, i + 1))) {
-				System.out.println("incrementing subsqtAmt (" + subsequentAmount + ")  because of: " + num.substring(i, i + 1));
+				//System.out.println("incrementing subsqtAmt (" + subsequentAmount + ")  because of: " + num.substring(i, i + 1));
 				subsequentAmount++;
 			} else {
 				subsequentAmount = 0;
